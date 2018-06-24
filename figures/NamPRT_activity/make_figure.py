0b1cb390f977fedd545a1a7dd7342052e2618ff9
Mathias Bockwoldt <mathias.bockwoldt@uit.no>
2018-06-24 15:14:21 +0200

    Modified NamPRT activity plot

--- figures/NamPRT_activity/make_figure.py
+++ figures/NamPRT_activity/make_figure.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
-import seaborn as sns
+import scipy.stats as stats
 
 fs = 9
 img_width = 4.5 # 3.42 or 4.5 or 7.0 for PNAS
@@ -20,7 +20,7 @@ plt.rc('figure', titlesize=fs) # fontsize of the figure title
 
 #color = sns.color_palette("colorblind", n_colors=2)
 
-majorlabel = ('wildtype', 'mutant')
+majorlabel = ('wt NamPRT', '$\Delta$42-51 NamPRT')
 minorlabel1 = ('Nam 1\nPRPP 1\nATP 0', '1\n0.1\n0', '0.1\n1\n0')
 minorlabel2 = ('0.1\n0.1\n0', '0.1\n0.1\n1')
 
@@ -28,11 +28,15 @@ wt1 = []
 mu1 = []
 wtstd1 = []
 mustd1 = []
+wtall1 = []
+muall1 = []
 
 wt2 = []
 mu2 = []
 wtstd2 = []
 mustd2 = []
+wtall2 = []
+muall2 = []
 
 with open('activity.csv') as f:
 	next(f)
@@ -48,6 +52,8 @@ with open('activity.csv') as f:
 		mu1.append(np.mean(mubuf))
 		wtstd1.append(np.std(wtbuf))
 		mustd1.append(np.std(mubuf))
+		wtall1.append(tuple(wtbuf))
+		muall1.append(tuple(mubuf))
 
 	rel = None
 	for _ in minorlabel2:
@@ -63,8 +69,10 @@ with open('activity.csv') as f:
 		mu2.append(np.mean(mubuf)/rel)
 		wtstd2.append(np.std(wtbuf)/rel)
 		mustd2.append(np.std(mubuf)/rel)
+		wtall2.append(tuple(wtbuf))
+		muall2.append(tuple(mubuf))
 
-sns.set_style("whitegrid")
+wtstd2[0] = 0 # disable error bar for first element in second subplot
 
 fig = plt.figure(figsize = (img_width, img_height))
 ax1 = fig.add_subplot(1,2,1) # numrows, numcols, fignum
@@ -82,22 +90,41 @@ offset = width/2
 wt_bar2 = ax2.bar(ind2+offset+offset, wt2, width, yerr=wtstd2, color='black', align='center', error_kw={'ecolor':'black'})
 mu_bar2 = ax2.bar(ind2+width+width, mu2, width, yerr=mustd2, color='grey', align='center', error_kw={'ecolor': 'black'})
 
-ax1.legend((wt_bar1[0], mu_bar1[0]), majorlabel, loc='upper right')
-#ax2.legend((wt_bar2[0], mu_bar2[0]), majorlabel, loc='upper left')
+ax1.legend((wt_bar1[0], mu_bar1[0]), majorlabel, loc='upper right', frameon=False)
+ax2.legend((wt_bar2[0], mu_bar2[0]), majorlabel, loc='upper left', frameon=False)
 
 ax1.xaxis.grid(False)
 ax1.set_xlim(0, 3)
+ax1.set_ylim(0, 11)
 ax1.set_xticks(ind1 + width + 0.165)
 ax1.set_xticklabels(minorlabel1)
-ax1.set_ylabel('NMN (nmol/mg enzyme/min)')
+#ax1.set_ylabel('NMN (nmol/mg enzyme/min)')
+ax1.set_ylabel('kcat (1/s)')
+ax1.xaxis.set_ticks_position('bottom')
+ax1.yaxis.set_ticks_position('left')
 
 ax2.xaxis.grid(False)
 ax2.set_xlim(0, 2)
+ax2.set_ylim(0, 3)
 ax2.set_xticks(ind2 + width + 0.165)
 ax2.set_xticklabels(minorlabel2)
-ax2.set_ylabel('NMN (relative)')
+ax2.set_ylabel('kcat (rel. to mean WT w/o ATP)')
+ax2.xaxis.set_ticks_position('bottom')
+ax2.yaxis.set_ticks_position('left')
 
 plt.tight_layout()
+
+
+print('T-test pvalues: Nam,PRPP,ATP')
+print('mu 1 vs. mu 3', stats.ttest_ind(muall1[0], muall1[2]).pvalue)
+print('mu 1 vs. mu 2', stats.ttest_ind(muall1[0], muall1[1]).pvalue)
+print('mu 2 vs. mu 3', stats.ttest_ind(muall1[1], muall1[2]).pvalue)
+print('wt 1 vs. wt 3', stats.ttest_ind(wtall1[0], wtall1[2]).pvalue)
+print('wt 1 vs. wt 2', stats.ttest_ind(wtall1[0], wtall1[1]).pvalue)
+print('wt 2 vs. wt 3', stats.ttest_ind(wtall1[1], wtall1[2]).pvalue)
+
+print('mu 4 vs. mu 5', stats.ttest_ind(muall2[0], muall2[1]).pvalue)
+print('wt 4 vs. wt 5', stats.ttest_ind(wtall2[0], wtall2[1]).pvalue)
 
 if len(sys.argv) > 1 and sys.argv[1].startswith('sh'):
 	plt.show()
